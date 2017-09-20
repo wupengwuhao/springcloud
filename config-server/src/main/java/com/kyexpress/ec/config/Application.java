70dd11dfc92e88a1d375dc3a1bad2a84cea2cabf
wupeng <1562015771@qq.com>
2017-09-20 18:12:43 +0800

    高可靠配置中心

--- config-server/src/main/java/com/kyexpress/ec/config/Application.java
+++ config-server/src/main/java/com/kyexpress/ec/config/Application.java
@@ -4,11 +4,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
