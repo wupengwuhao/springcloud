e79ceaa5cee08e327d53339c4ec123e4e6ae6323
wupeng <1562015771@qq.com>
2017-09-20 16:45:41 +0800

    配置中心

--- compute-service/src/main/java/com/kyexpress/ec/compute/Application.java
+++ compute-service/src/main/java/com/kyexpress/ec/compute/Application.java
@@ -1,7 +1,7 @@
 package com.kyexpress.ec.compute;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
@@ -9,7 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class Application {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(Application.class).web(true).run(args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
